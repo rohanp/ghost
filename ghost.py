cbd68ecaf14aaadf049c66b45b06d73b0aeae7aa
Rohan Pandit <pandit.rohan@gmail.com>
2014-12-11 15:43:39 -0500

    2 player ghost working

--- ghost.py
+++ ghost.py
@@ -1,7 +1,23 @@
+#######################################
+# 	 ____       _                    #
+#	|  _ \ ___ | |__   __ _ _ __     #
+#	| |_) / _ \| '_ \ / _` | '_ \    #
+#	|  _ < (_) | | | | (_| | | | |   #
+#	|_| \_\___/|_| |_|\__,_|_| |_|   #
+#			  Period 1				 #
+#			   12/9/14				 #
+######################################
+
 import sys
 from time import clock
-import itertools
 from random import random
+from itertools import zip_longest
+import pickle
+import sys
+from time import sleep
+
+sys.setrecursionlimit(10000)
+N=2
 
 class Node:
 	def __init__(self,value):
@@ -32,7 +48,7 @@ class Node:
 			self.children[s[0]]=p
 			p.insert(s[1:])
 
-	def search(self, s):
+	"""def search(self, s):
 		return self.searchHelper(s+'$')
 
 	def searchHelper(self, s):
@@ -44,17 +60,6 @@ class Node:
 		except IndexError:
 			return False
 		return False
-
-	"""boring search
-	def search(self, s):
-		if '$':
-			return True
-		elif s[0] in self.children:
-			return self.children[s[0]].searchHelper(s[1:])
-
-		return False
-
-	"""
 
 	def searchFrag(self, s):
 		if s=='':
@@ -71,7 +76,14 @@ class Node:
 				return self
 			else:
 				return self.children[s[0]].searchFrag(s[1:])
-		return null
+		return null"""
+
+	def mustSpellWord(self):
+		if 1==len(self.children.keys()):
+			lastLetter = list(self.children.values())[0]
+			if 1==len(lastLetter.children) and '$'==list(lastLetter.children.keys())[0]:
+				return True
+		return False
 
 	def display(self):
 		if not self.children:
@@ -93,7 +105,7 @@ class Node:
 
 		# Builds up the brace, using the child midpoints
 		brace_builder = []
-		for i in xrange(display_width):
+		for i in range(display_width):
 			if i < child_midpoints[0] or i > child_midpoints[-1]:
 				brace_builder.append(' ')
 			elif i in child_midpoints:
@@ -118,7 +130,7 @@ def stack_str_blocks(blocks):
 	block_lens = [block_width(bl) for bl in blocks]
 	split_blocks = [bl.split('\n') for bl in blocks]
 
-	for line_list in itertools.izip_longest(*split_blocks, fillvalue=None):
+	for line_list in zip_longest(*split_blocks, fillvalue=None):
 		for i, line in enumerate(line_list):
 			if line is None:
 				builder.append(' ' * block_lens[i])
@@ -132,27 +144,85 @@ def stack_str_blocks(blocks):
 
 def basics():
 	root=Node('*')
-	root.insert('cat')
-	root.insert('catnip')
-	root.insert('cats')
-	root.insert('catnap')
-	root.insert("can't")
-	root.insert('cat-x')
-	root.insert('dog')
-	root.insert('dogs')
-	root.insert('dognip')
-	print(root)
+	root.insert('random')
+	root.insert('royal')
+	root.insert('rower')
+	root.insert('rowing')
+	root.insert("runner")
+	root.insert('running')
+	root=root.children['r']
 	print(root.display())
-	print(root.search('junk'))
-	print(root.search('dogs'))
-	print(root.searchFrag('do'))
-	print(root.searchFrag('dox'))
+	losers = [loser(o,2) for o in root.children.values()]
+	print(losers)
+
+
+def loser(root, player):
+	nextPlayer = player+1
+	if nextPlayer > N:
+		nextPlayer=1
+
+	if root.mustSpellWord():
+		return nextPlayer
+
+	for o in root.children.values():
+		if loser(o, nextPlayer) != nextPlayer:
+			return player
+
+	return nextPlayer
+
+def computerMove(root, s):
+	options = list(root.children.values())
+	for o in options:
+		if '$' not in o.children.keys():
+			choice=o.val
+			break
+	else:
+		choice=options[0].val
+	s += choice
+	print("Computer chooses character %s"%choice)
+	root = root.children[choice]
+
+	if '$' in root.children and len(s)>3:
+		print("Computer loses!", s, " is a word.")
+		exit()
+
+	print("word: %s \n"%s)
+	return root, s
 
 def humanMove(root, s):
-	c=input("Human Turn. Enter a charater: ").lower()[0]
+	c=input("Human Turn. Enter a charater:").lower()[0]
 	#print(root)
+	while c == '?':
+		start=clock()
+		validMoves = ', '.join(root.children.keys())
+		print("valid moves = ", validMoves,)
+		print("calculated in ", clock()-start, "s \n")
+
+		start=clock()
+		times=[]
+		validMoves=root.children.values()
+		for o in validMoves:
+			miniStart=clock()
+			if 1 != loser(o,1):
+				times.append(1000)
+			else:
+				times.append(round((clock()-miniStart)*10000, 2))
+		validMoves=[move.val for move in validMoves]
+		sortedMoves = ', '.join([move for (t, move) in reversed(sorted(zip(times,validMoves)))])
+		print("sorted valid moves ", sortedMoves)
+		print("calculated in ", clock()-start, '\n')
+
+		start=clock()
+		validMoves=root.children
+		optimalMoves = [o.val for o in validMoves.values() if loser(o, 1) != 1]
+		optimalMoves = ', '.join(list(optimalMoves))
+		print("optimal moves = ", optimalMoves)
+		print("calculated in ", clock()-start, "s \n")
+		c=input("Human Turn. Enter a charater: ").lower()[0]
 	s+=c
-	if '$' in root.children and len(s)>3:
+
+	choice = root.children[c]
+	if '$' in choice.children.keys() and len(s)>3:
 		print("Human loses. %s is a word"%s)
 		exit()
 	elif not c in root.children:
@@ -163,28 +233,13 @@ def humanMove(root, s):
 		root=root.children[s[len(s)-1]]
 		return root, s
 
-def computerMove(root, s):
-	options = list(root.children.keys())
-	#print(options)
-	choice = options[0]
-	s += choice
-	print("Computer chooses character %s"%choice)
-	#print(s)
-	root = root.children[choice]
-	#print(root)
-
-	if '$' in root.children and len(s)>3:
-		print("Computer loses!", s, " is a word.")
-		exit()
-
-	print("word: %s \n"%s)
-	return root, s
-
 def main():
 	f = open("ghostDictionary.txt")
 	root=Node('*')
 	for line in f:
-		root.insert(line.lower().strip())
+		s = line.lower().strip()
+		if 3<len(s):
+			root.insert(line.lower().strip())
 
 	f.close()
 	s=''
